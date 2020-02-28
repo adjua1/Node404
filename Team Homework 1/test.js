1ee666e74692e1253d0f9dd1ed3fc8e5502d56e5
Joesniff44 <58479896+Joesniff44@users.noreply.github.com>
2020-02-27 20:08:32 -0500

    Add files via upload

--- Team Homework 1/test.js
+++ Team Homework 1/test.js
@@ -2,4 +2,32 @@
 
 var test = require('./test.js');
 
-console.log(test.getGPA('A', 'A', 'A', 'A'));
+//console.log(test.getGPA('A', 'A', 'A', 'A'));
+
+const testCases = [['A', 'A', 'A', 'A', 4], ['B', 'B', 'B', 'B', 3], ['C', 'C', 'C', 'C', 2], ['D', 'D', 'D', 'D', 1], ['F', 'F', 'F', 'F', 0], ['A', 'A', 'A', 'Q', "The value given Q is not a valid input"]];
+const results = [];
+
+var i;
+
+for(i = 0; i < testCases.length; i++)
+{
+    results.push([testCases[i][4], (test.getGPA(testCases[i][0],testCases[i][1],testCases[i][2],testCases[i][3]))]);
+}
+
+console.log(results);
+
+var j;
+var errors = 0;
+var errorCases = [];
+for(j = 0; j < results.length; j++)
+{
+    if(results[j][0] != results[j][1])
+    {
+        errors += 1;
+        errorCases.push(testCases[j]);
+    }
+}
+
+var errorRate = (errors/(results.length))*100;
+
+console.log("We have an error rate of "+ errorRate + "%. The error cases are as follows:\n" + errorCases);
